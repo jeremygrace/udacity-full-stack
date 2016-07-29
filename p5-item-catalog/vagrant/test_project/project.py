db428620d5301500cbbec80028bd95d2281aacf0
Jeremy Grace <jeremy.grace@outlook.com>
2016-07-28 21:23:37 -0400

    Update newMenuItem function for Flask app

--- p5-item-catalog/vagrant/test_project/project.py
+++ p5-item-catalog/vagrant/test_project/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup2 import Base, Restaurant, MenuItem
@@ -23,13 +23,22 @@ def restaurantMenu(restaurant_id):
 	return render_template('menu.html', restaurant=restaurant, items=items)
 	
 
-@app.route('/restaurants/<int:restaurant_id>/new/')
+@app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
-	return "page ro create a new menu item. Task 1 COMPLETE!"
+	if request.method == 'POST':
+		newItem = MenuItem(
+			name=request.form['name'], restaurant_id=restaurant_id)
+		session.add(newItem)
+		session.commit()
+		return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+	else:
+		return render_template('newmenuitem.html', restaurant_id=restaurant_id)
+
 
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
 def editMenuItem(restaurant_id, menu_id):
 	return "page to edite a menu item. Task 2 COMPLETE!"
+
 
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/')
 def deleteMenuItem(restaurant_id, menu_id):
