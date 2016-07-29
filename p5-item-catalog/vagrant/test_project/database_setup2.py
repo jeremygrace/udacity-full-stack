c0988479d4e47b11a25382dc3062811b7e54c66d
Jeremy Grace <jeremy.grace@outlook.com>
2016-07-29 16:58:06 -0400

    Update all python files with complete Flask app code

--- p5-item-catalog/vagrant/test_project/database_setup2.py
+++ p5-item-catalog/vagrant/test_project/database_setup2.py
@@ -13,6 +13,15 @@ class Restaurant(Base):
 	name = Column(String(80), nullable = False)
 	id = Column(Integer, primary_key = True)
 
+	@property
+	def serialize(self):
+		"""Return object data in easily serializable format
+		"""
+		return {
+			'name' : self.name,
+			'id' : self.id
+		}
+	
 
 class MenuItem(Base):
 	__tablename__ = 'menu_item'
@@ -24,6 +33,19 @@ class MenuItem(Base):
 	price = Column(String(8))
 	restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
 	restaurant = relationship(Restaurant)
+
+	@property
+	def serialize(self):
+		"""Returns ojbect data in easily serializeable format
+		"""
+		return {
+			'name' : self.name,
+			'description' : self.description,
+			'id' : self.id,
+			'price' : self.price,
+			'course' : self.course
+		}
+	
 
 
 engine = create_engine('sqlite:///restaurantmenu.db')
